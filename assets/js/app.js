7fe588bb754e16e67a989d0317fc0e6dfce03cdc
Kitsune7X <ryuizanagi@proton.me>
2025-10-24 02:32:41 +0900

    feat: finish main function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,85 +1,123 @@
-// Numbers of Rounds
-const ROUNDS = 5;
+// -------------------------------
+// Variables declaration
+// -------------------------------
+// Win Score
+const WIN_SCORE = 5;
+// Game state
+let isGameOver = false;
 
-// Rock, Paper, Scissors array
-const hand = ["ROCK", "PAPER", "SCISSORS"];
+// AI Pokemon sprites
+const aiPokemon = document.querySelector("#ai-pokemon img");
+// AI Score
+const aiScore = document.querySelector("#ai-score span");
+// AI Score Value
+let aiScoreValue = 0;
 
-// Human score
-let humanScore = 0;
-// Computer score
-let computerScore = 0;
+// AI Pokemon Array
+const aiPokemonArray = [
+  { name: "Grass", path: "../assets/images/1.gif" },
+  { name: "Fire", path: "../assets/images/4.gif" },
+  { name: "Water", path: "../assets/images/7.gif" },
+];
 
-// Get Computer Choice Function
-function getComputerChoice() {
-  return hand[Math.floor(Math.random() * hand.length)];
+// Player Pokemon sprites
+const playerPokemon = document.querySelector("#player-pokemon img");
+// Player Score
+const playerScore = document.querySelector("#player-score span");
+// Player Score Value
+let playerScoreValue = 0;
+
+// Player Pokemon Array
+const playerPokemonArray = [
+  { name: "Grass", path: "../assets/images/back/1.gif" },
+  { name: "Fire", path: "../assets/images/back/4.gif" },
+  { name: "Water", path: "../assets/images/back/7.gif" },
+];
+
+// Player Choices container
+const playerChoices = document.querySelector("#pokemon-box");
+
+// Message Overlay
+const message = document.querySelector("#game-screen-overlay span");
+
+// Get Random AI Choice
+function getAIChoice(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  aiPokemon.setAttribute("src", arr[index].path);
+  return arr[index].name;
 }
 
-// // Get Human Choice
-// function getHumanChoice() {
-//     return prompt("Your hand?");
-// }
+// Game State
 
-// Play round function
-function playRound(humanChoice, computerChoice) {
-  switch (humanChoice) {
-    case "ROCK":
-      if (computerChoice === "ROCK") {
-        console.log("TIE!");
-      } else if (computerChoice === "PAPER") {
-        computerScore++;
-        console.log("You LOSE! Paper beats Rock.");
-      } else {
-        humanScore++;
-        console.log("You WIN! Rock beats Scissors.");
+// Get Player Choice
+playerChoices.addEventListener(
+  "click",
+  (e) => {
+    let target = e.target;
+    const aiChoice = getAIChoice(aiPokemonArray);
+
+    if (target.id === "grass" || target.id === "btnGrass") {
+      playerPokemon.setAttribute("src", playerPokemonArray[0].path);
+      switch (aiChoice) {
+        case "Grass":
+          message.textContent = "It's a TIE!";
+          break;
+        case "Fire":
+          aiScoreValue++;
+          aiScore.textContent = `AI: ${aiScoreValue}`;
+          message.textContent = "It's NOT very EFFECTIVE!";
+          break;
+        case "Water":
+          playerScoreValue++;
+          playerScore.textContent = `PLAYER: ${playerScoreValue}`;
+          message.textContent = "It's SUPER EFFECTIVE!";
+          break;
       }
-      break;
-    case "PAPER":
-      if (computerChoice === "ROCK") {
-        humanScore++;
-        console.log("You WIN! Paper beats Rock.");
-      } else if (computerChoice === "PAPER") {
-        console.log("TIE!");
-      } else {
-        computerScore++;
-        console.log("You LOSE! Scissors beats Paper.");
+    } else if (target.id === "fire" || target.id === "btnFire") {
+      playerPokemon.setAttribute("src", playerPokemonArray[1].path);
+      switch (aiChoice) {
+        case "Grass":
+          playerScoreValue++;
+          playerScore.textContent = `PLAYER: ${playerScoreValue}`;
+          message.textContent = "It's SUPER EFFECTIVE!";
+          break;
+        case "Fire":
+          message.textContent = "It's a TIE!";
+          break;
+        case "Water":
+          aiScoreValue++;
+          aiScore.textContent = `AI: ${aiScoreValue}`;
+          message.textContent = "It's NOT very EFFECTIVE!";
+          break;
       }
-      break;
-    case "SCISSORS":
-      if (computerChoice === "ROCK") {
-        computerScore++;
-        console.log("You LOSE! Rock beats Scissors.");
-      } else if (computerChoice === "PAPER") {
-        humanScore++;
-        console.log("You WIN! Scissors beat Paper.");
-      } else {
-        console.log("TIE!");
+    } else {
+      playerPokemon.setAttribute("src", playerPokemonArray[2].path);
+      switch (aiChoice) {
+        case "Grass":
+          aiScoreValue++;
+          aiScore.textContent = `AI: ${aiScoreValue}`;
+          message.textContent = "It's NOT very EFFECTIVE!";
+          break;
+        case "Fire":
+          playerScoreValue++;
+          playerScore.textContent = `PLAYER: ${playerScoreValue}`;
+          message.textContent = "It's SUPER EFFECTIVE!";
+          break;
+        case "Water":
+          message.textContent = "It's a TIE!";
       }
-      break;
-  }
-  console.log(`🙂 Human Score: ${humanScore}`);
-  console.log(`💻 Computer Score: ${computerScore}`);
-}
+    }
 
-// Play Game function
-function playGame() {
-  for (let i = 0; i < ROUNDS; i++) {
-    // Human Hand
-    const humanHand = getHumanChoice().toUpperCase();
-
-    // Computer Hand
-    const computerHand = getComputerChoice();
-
-    playRound(humanHand, computerHand);
-  }
-
-  if (humanScore > computerScore) {
-    console.log("🙂 Human WINs!");
-  } else if (humanScore < computerScore) {
-    console.log("💻 Computer WINs!");
-  } else {
-    console.log("🙂 TIE! 💻");
-  }
-}
-
-// Activate Game
-playGame();
+    if (playerScoreValue === WIN_SCORE) {
+      isGameOver = true;
+      message.textContent = "PLAYER WINS!";
+      return isGameOver;
+    }
+    if (aiScoreValue === WIN_SCORE) {
+      isGameOver = true;
+      message.textContent = "AI WINS!";
+      return isGameOver;
+    }
+  },
+  { capture: true }
+);
